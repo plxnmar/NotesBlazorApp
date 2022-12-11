385c32c11dafb329f3c571a4db55649e0dc4cd54
Mira0Kit <kitomura@bk.ru>
2022-12-12 01:41:27 +0300

    Добавьте файлы проекта.

---
+++ NotesBlazorApp/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using NotesBlazorApp.Server.Models;
+
+namespace NotesBlazorApp.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
