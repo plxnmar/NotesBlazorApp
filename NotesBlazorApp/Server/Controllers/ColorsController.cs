19bbec048fe5cdbfb0cfe18f14e4b212ae3a1cb2
Mira0Kit <kitomura@bk.ru>
2022-12-12 01:50:07 +0300

    добавлены страницы и апи

---
+++ NotesBlazorApp/Server/Controllers/ColorsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Notes.BLL.Interfaces;
+using Notes.BLL.Services;
+using Notes.Domain.Entities;
+using System.Collections.Generic;
+
+namespace Notes.Server.Controllers
+{
+    [Route("api/colors")]
+    [ApiController]
+    public class ColorsController : ControllerBase
+    {
+        private readonly IColorService _colorService;
+
+        public ColorsController(IColorService colorService)
+        {
+            this._colorService = colorService;
+        }
+
+        [HttpGet]
+        public IEnumerable<ColorCard> GetAll()
+        {
+            return _colorService.GetColors();
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            ColorCard color = await _colorService.GetColor(id);
+            if (color != null)
+            {
+                return Ok(color);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ NotesBlazorApp/Server/Controllers/NotesController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Notes.BLL.Interfaces;
+using Notes.Domain.Entities;
+using System.Collections.Generic;
+
+namespace Notes.Server.Controllers
+{
+    [Route("api/notes")]
+    [ApiController]
+    public class NotesController : ControllerBase
+    {
+        private readonly INoteService _noteService;
+
+        public NotesController(INoteService noteService)
+        {
+            this._noteService = noteService;
+        }
+
+        [HttpGet]
+        public IEnumerable<Note> GetAll()
+        {
+            return _noteService.GetNotes();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            Note note = await _noteService.GetNote(id);
+            if (note != null)
+            {
+                return Ok(note);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public void Post(Note note)
+        {
+            _noteService.AddNote(note);
+        }
+
+        [HttpPut]
+        public void Put(Note note)
+        {
+            _noteService.UpdateNote(note);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _noteService.DeleteNote(id);
+            return Ok();
+        }
+
+    }
+}
